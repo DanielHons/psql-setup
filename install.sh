4c156ecaef532d692f008055003dbafed152eca0
Daniel Hons <mail@danielhons.de>
2020-03-25 11:52:16 +0100

    source in the correct shell session

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-source CONFIG
 
 apt-get update
 apt install postgresql postgresql-client -y
@@ -8,10 +7,11 @@ apt install postgresql postgresql-client -y
 sed -i -e 's/#listen_addresses/listen_addresses/' /etc/postgresql/11/main/postgresql.conf
 systemctl restart postgresql
 
-# Just to avoid an error message because user postgres operating in roots home directory
-cd /tmp
 
 su -s /bin/bash postgres
+source CONFIG
+# Just to avoid an error message because user postgres operating in roots home directory
+cd /tmp
 psql -v ON_ERROR_STOP=1  <<-EOSQL
     CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';
     CREATE DATABASE $POSTGRES_DATABASE;
